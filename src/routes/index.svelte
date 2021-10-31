8deb89f827709c3e820acf994a0112d41b100037
jacktanlol <ejacktanjack@gmail.com>
2021-10-31 05:13:57 +0000

    dark

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,7 +25,7 @@
 </script>
 
 
-<div class=" g-dark text-white text-center">
+<div class=" bg-dark text-white text-center">
     <Header {title}/>
     <Section {...myInfo}/>
     <Section {...myHobby}/>
